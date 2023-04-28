03bfe107d377ccd31f22056c17e17b5970134b77
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2023-04-28 23:11:44 +0200

    feat: get content from config

---
+++ site.config.js
@@ -0,0 +1,75 @@
+export default {
+  title: "jigglycrumb.space",
+  content: [
+    {
+      headline: "Where to find me",
+      links: [
+        {
+          title: "Github",
+          icon: "/github.svg",
+          href: "https://github.com/jigglycrumb",
+        },
+        {
+          title: "Redbubble",
+          icon: "/redbubble.svg",
+          href: "https://bubblepleb.redbubble.com",
+        },
+        {
+          title: "Nostr",
+          icon: "/nostr.svg",
+          href: "https://nosta.me/416ca193aa5448b8cca1f09642807765cc0ee299609f972df0614cfb8ea2f2b1",
+        },
+      ],
+    },
+    {
+      headline: "My websites",
+      links: [
+        {
+          title: "nostr.industries",
+          href: "https://nostr.industries",
+          body: "Free nostr address provider",
+        },
+        {
+          title: "bitcoin-lexikon.org",
+          href: "http://bitcoin-lexikon.org",
+          body: "Das kleine Bitcoin Lexikon",
+        },
+        {
+          title: "sleekspriter.app",
+          href: "https://sleekspriter.app",
+          body: "Pixel art editor",
+        },
+        {
+          title: "asciimoji.com",
+          href: "https://asciimoji.com",
+          body: "ASCII emoticons for Mac OS/iOS and more",
+        },
+      ],
+    },
+    {
+      headline: "Side projects",
+      links: [
+        {
+          title: "splash-ds",
+          href: "https://jigglycrumb.github.io/splash-ds/",
+          body: "Boot splash creator for Nintendo 3DS",
+        },
+        {
+          title: "TvTool",
+          href: "https://jigglycrumb.github.io/tvtool/",
+          body: "Tool for formatting TV show episode names",
+        },
+        {
+          title: "Space Bacon",
+          href: "https://jigglycrumb.github.io/space-bacon/",
+          body: "Tiny game featuring my cat",
+        },
+        {
+          title: "What would a pirate do?",
+          href: "https://jigglycrumb.github.io/what-would-a-pirate-do/",
+          body: "Decision making tool for pirates",
+        },
+      ],
+    },
+  ],
+};
